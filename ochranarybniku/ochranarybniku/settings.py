9997f8771ef2bbd75ca7c097433063c7de545a97
lubos <lubos.pechar@prirodou.cz>
2022-05-24 19:14:26 +0200

    zapnutí geo rozšíření djanga

--- ochranarybniku/ochranarybniku/settings.py
+++ ochranarybniku/ochranarybniku/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'ponds',
 ]
 
