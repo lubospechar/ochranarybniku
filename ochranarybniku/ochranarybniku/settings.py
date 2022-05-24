55f082d9e90b89b452740559159d2aced3f164ee
lubos <lubos.pechar@prirodou.cz>
2022-05-24 22:01:32 +0200

    přidání STATIC_ROOT do settings.py

--- ochranarybniku/ochranarybniku/settings.py
+++ ochranarybniku/ochranarybniku/settings.py
@@ -127,8 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = config('STATIC_ROOT') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
