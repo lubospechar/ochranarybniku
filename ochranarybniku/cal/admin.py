3a09f58b47f7f8345036021096a138ab0237022d
lubos <lubos.pechar@prirodou.cz>
2022-05-24 20:16:07 +0200

    přidán admin pro cal:PondVisit

--- ochranarybniku/cal/admin.py
+++ ochranarybniku/cal/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from cal.models import PondVisit
 
-# Register your models here.
+@admin.register(PondVisit)
+class PondVisitAdmin(admin.ModelAdmin):
+    list_display = ('pond' ,'desc', 'dt_start', 'dt_end')
+    exclude = ('user', )
+    
+    def save_form(self, request, form, change):
+        obj = super().save_form(request, form, change)
+        obj.user = request.user
+        return obj
+    
