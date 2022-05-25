7e952cda6588f24bfade23fa6b310a315b11a566
lubos <lubos.pechar@prirodou.cz>
2022-05-25 08:45:54 +0200

    centrování mapy při vyplňování rybníku na ČR

--- ochranarybniku/ponds/admin.py
+++ ochranarybniku/ponds/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from django.contrib.gis.admin import GISModelAdmin
+from django.contrib.gis.admin import OSMGeoAdmin
+from django.contrib.gis.geos import Point
 from ponds.models import Pond
 
+
 @admin.register(Pond)
-class PondAdmin(GISModelAdmin):
-    pass
+class PondAdmin(OSMGeoAdmin):
+    pnt = Point(15.3,50, srid=4326)
+    pnt.transform(900913)
+    default_lon, default_lat = pnt.coords
+    default_zoom=7
+    map_width = 900 
+    map_height = 650
