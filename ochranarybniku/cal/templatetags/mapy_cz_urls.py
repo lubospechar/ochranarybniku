e00a2185c57a61bfc5f763a7e3d923e28e4c8551
lubos <lubos.pechar@prirodou.cz>
2022-05-25 15:47:06 +0200

    formátování kódu beze změny funkčnosti

--- ochranarybniku/cal/templatetags/mapy_cz_urls.py
+++ ochranarybniku/cal/templatetags/mapy_cz_urls.py
@@ -1,9 +1,11 @@
 from django import template
 from django.utils.html import mark_safe
+
 register = template.Library()
+
 
 @register.simple_tag
 def mapy_cz_turist(x, y, name, zoom):
-    return mark_safe(f'<a href="https://mapy.cz/turisticka?x={x}&amp;y={y}&amp;z={zoom}">{name}</a>')
-
-
+    return mark_safe(
+        f'<a href="https://mapy.cz/turisticka?x={x}&amp;y={y}&amp;z={zoom}">{name}</a>'
+    )
