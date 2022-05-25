d20a8015a43ffa79fb71af3916a942bad06205a0
lubos <lubos.pechar@prirodou.cz>
2022-05-25 14:36:13 +0200

    View a template pro kalenář návštěv rybníků, prozatím jako home.html

--- ochranarybniku/cal/views.py
+++ ochranarybniku/cal/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from cal.models import PondVisit
+import datetime
 
 def home(request):
-    visits = PondVisit.objects.all()
-    return render(request, 'cal/home.html', {'visits': visits})
+    past_visits = PondVisit.objects.filter(
+        dt_end__lte=datetime.datetime.now()
+    )
+    
+    future_visits = PondVisit.objects.filter(
+        dt_end__gte=datetime.datetime.now()
+    )
+    
+    return render(request, 'cal/home.html', {
+        'past_visits': past_visits,
+        'future_visits': future_visits,
+    })
     
