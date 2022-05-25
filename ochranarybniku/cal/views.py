e23c29320f44f661569f212fb3a9c38dbc56b386
lubos <lubos.pechar@prirodou.cz>
2022-05-25 15:40:23 +0200

    vyměna timezone za datetime

--- ochranarybniku/cal/views.py
+++ ochranarybniku/cal/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
+from django.utils import timezone
 from cal.models import PondVisit
-import datetime
 
 def home(request):
     past_visits = PondVisit.objects.filter(
-        dt_end__lte=datetime.datetime.now()
+        dt_end__lte=timezone.now()
     )
     
     future_visits = PondVisit.objects.filter(
-        dt_end__gte=datetime.datetime.now()
+        dt_end__gte=timezone.now()
     )
     
     return render(request, 'cal/home.html', {
