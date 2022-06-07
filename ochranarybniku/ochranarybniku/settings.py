a77da9747a69ccb1327cb927a9d95b4a44f1327d
lubospechar <lubos.pechar@prirodou.cz>
2022-06-07 20:01:44 +0200

    příprava na jazykové mutace

--- ochranarybniku/ochranarybniku/settings.py
+++ ochranarybniku/ochranarybniku/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -120,6 +121,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
 LANGUAGE_CODE = 'cs'
+
+LANGUAGES = (
+    ('cs', 'Czech'),
+    ('en', 'English'),
+)
 
 TIME_ZONE = 'Europe/Prague'
 
--- ochranarybniku/webapp/views.py
+++ ochranarybniku/webapp/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.utils import translation
+from django.conf import settings
+
+def set_language(request, language):
+    user_language = language
+    translation.activate(language)
+    request = redirect('home')
+    request.set_cookie(settings.LANGUAGE_COOKIE_NAME, user_language)
+    return request
+
 
 @login_required
 def home(request):
