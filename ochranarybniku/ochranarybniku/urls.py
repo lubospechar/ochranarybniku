8af2a0a79899ad675523a882d856911485031af5
lubos <lubos.pechar@prirodou.cz>
2022-06-14 16:10:52 +0200

    dpřidání debug url pro media

--- ochranarybniku/ochranarybniku/urls.py
+++ ochranarybniku/ochranarybniku/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.utils.html import format_html
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webapp.urls')),
     path('kalendar', include('cal.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(), name="login"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 # hlavička v administraci a jejím loginu
