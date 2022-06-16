409e44896746b230d9fd73ea2956b06c8cd633b3
lubospechar <lubos.pechar@prirodou.cz>
2022-06-16 15:54:45 +0200

    doplnění filtrů a zobrazení pro fotogalerie

--- ochranarybniku/webapp/admin.py
+++ ochranarybniku/webapp/admin.py
@@ -7,6 +7,7 @@ class PictureInline(admin.TabularInline):
     model = Picture
     prepopulated_fields = {
         "slug_cs": ("description_cs",),
+        "slug_en": ("description_en",),
     }
     thumbnail = AdminThumbnail(image_field="admin_thumbnail")
     thumbnail.short_description = "Náhled"
@@ -15,9 +16,16 @@ class PictureInline(admin.TabularInline):
 
 @admin.register(PhotoGallery)
 class PhotoGalleryAdmin(admin.ModelAdmin):
-    list_display = ("name_cs", "description_cs", "count_pictures")
-    list_filter = ("created", "modified")
-    search_fields = ("name_cs", "description_cs")
+    list_display = (
+        "name_cs",
+        "name_en",
+        "description_cs",
+        "description_en",
+        "count_pictures",
+        "enable",
+    )
+    list_filter = ("enable", "created", "modified")
+    search_fields = ("name_cs", "description_cs", "name_en", "description_en")
     inlines = [
         PictureInline,
     ]
@@ -30,6 +38,8 @@ class PhotoGalleryAdmin(admin.ModelAdmin):
 @admin.register(Picture)
 class PictureAdmin(admin.ModelAdmin):
     list_display = ("__str__", "thumbnail")
+    search_fields = ("description_cs", "description_en", "enable")
+    list_filter = ("enable",)
     thumbnail = AdminThumbnail(image_field="admin_thumbnail")
     thumbnail.short_description = "Náhled"
     prepopulated_fields = {
