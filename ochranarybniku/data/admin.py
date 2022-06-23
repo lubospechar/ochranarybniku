324ab5160f3853c590ac44192c1115ceba554d9f
lubospechar <lubos.pechar@prirodou.cz>
2022-06-23 14:55:26 +0200

    Založení adminu pro Unit a PondMeasurement

--- ochranarybniku/data/admin.py
+++ ochranarybniku/data/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from data.models import (Unit, PondMeasurement)
 
-# Register your models here.
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+    list_display = ('shortcut', 'description')
+
+@admin.register(PondMeasurement)
+class PondMeasurementAdmin(admin.ModelAdmin):
+    list_display = ('date', 'pond')
