3bc543d9c5c1ca3112acff724ba1d0bc7a2a46f9
Lubos Pechar <lubos.pechar@prirodou.cz>
2023-04-19 15:01:50 +0200

    zachovani starych URL rybniku kvuli QR kodum

--- ochranarybniku/ochranarybniku/urls.py
+++ ochranarybniku/ochranarybniku/urls.py
@@ -9,6 +9,19 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('set-language/<str:language>/', set_language, name="set_language"),
+
+    # stare url kvuli QR kodum
+    path('rybniky/vizir/', RedirectView.as_view(url='/cs/rybnik/vizir')),
+    path('rybniky/kolvin-1/', RedirectView.as_view(url='/cs/rybnik/kolvin-1')),
+    path('rybniky/ledvinka/', RedirectView.as_view(url='/cs/rybnik/ledvinka')),
+    path('rybniky/gricak/', RedirectView.as_view(url='/cs/rybnik/gricak')),
+    path('rybniky/sindelka/', RedirectView.as_view(url='/cs/rybnik/sindelka')),
+    path('rybniky/velka-zabka/', RedirectView.as_view(url='/cs/rybnik/velka-zabka')),
+    path('rybniky/mala-zabka/', RedirectView.as_view(url='/cs/rybnik/mala-zabka')),
+    path('rybniky/vytaznik/', RedirectView.as_view(url='/cs/rybnik/vytaznik')),
+    path('rybniky/prazdny/', RedirectView.as_view(url='/cs/rybnik/prazdny')),
+    path('rybniky/prostredni/', RedirectView.as_view(url='/cs/rybnik/prostredni')),
+
     path("", RedirectView.as_view(url='set-language/cs/')), # tady by měla být autodetekce od prohlížeče
 
 
@@ -19,6 +32,7 @@ urlpatterns = [
     path("<str:lang>/", include("ponds.urls")),
     path("<str:lang>/", include("ponds.urls")),
     path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
+
 ]
 
 if settings.DEBUG:
